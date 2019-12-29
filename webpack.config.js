8d492e6fabbc1342c767b20f80601c6d69bea157
Aditprayogo <adit.ihzar@gmail.com>
2019-12-29 19:13:33 +0700

    adding webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+{
+    const webpack = require('webpack');
+    const path = require('path');
+
+    const config = {
+        entry: './src/js/index.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: 'babel-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        }
+    };
+
+    module.exports = config;
+}
