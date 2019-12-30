7fec92bfe73a3ef347e049af015c4477de0177f0
Aditprayogo <adit.ihzar@gmail.com>
2019-12-30 22:15:10 +0700

    configuration done for babel

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 // Global app controller
 import num from './test';
 
+const x = 23;
 
-console.log(`i imported ${num}  for another module`);
+console.log(`i imported ${num}  for another module called test.js variable x is ${x}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,37 @@
 {
     const webpack = require('webpack');
     const path = require('path');
+    const HtmlWebpackPlugin = require('html-webpack-plugin');
 
     const config = {
         entry: './src/js/index.js',
         output: {
-            path: path.resolve(__dirname, 'dist/js'),
-            filename: 'bundle.js'
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'js/bundle.js'
         },
-
         module: {
             rules: [
                 {
                     test: /\.js$/,
-                    use: 'babel-loader',
+                    use: {
+                        loader: 'babel-loader',
+                    },
                     exclude: /node_modules/
                 }
             ]
-        }
+        },
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            compress: true,
+            port: 9000
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'My App',
+                filename: 'index.html',
+                template: './src/index.html'
+            })
+        ]
     };
 
     module.exports = config;
