09798043a85c62340f91b680c883f7c065918129
Aditprayogo <adit.ihzar@gmail.com>
2020-01-01 02:09:42 +0700

    able to display result in search views

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
-import Search from './models/Search'
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 // global state
 const state = {};
@@ -6,20 +8,20 @@ const state = {};
 const controllerSearch = async () => {
 
     // 1 .) get query from the view
-    const query = 'pizza';
+    const query = searchView.getInput();
 
     if (query) {
-        // 2. ) New search object and add it to state
 
+        // 2. ) New search object and add it to state
         state.search = new Search(query);
 
         //3) prepare UI for result
 
         //4.) search for recipe
-        await state.search.getResult()
+        await state.search.getResult();
 
         //5.) render result on UI
-        console.log(state.search.result)
+        searchView.renderResults(state.search.result);
     }
 
 
@@ -27,7 +29,7 @@ const controllerSearch = async () => {
 
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
 
     e.preventDefault();
 
---
+++ src/js/views/base.js
@@ -0,0 +1,8 @@
+export const elements = {
+
+    searchInput: document.querySelector('.search__field'),
+    searchForm: document.querySelector('.search'),
+    searchResultList: document.querySelector('.results__list')
+
+
+};
