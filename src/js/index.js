566b2beddac73422dbaf8e1ee6bbb09f3370f7ce
Aditprayogo <adit.ihzar@gmail.com>
2020-01-01 00:46:08 +0700

    successfull search recipe from search components

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,37 @@
-// Global app controller
-import num from './test';
+import Search from './models/Search'
 
-const x = 23;
+// global state
+const state = {};
 
-console.log(`i imported ${num}  for another module called test.js variable x is ${x}`);
+const controllerSearch = async () => {
+
+    // 1 .) get query from the view
+    const query = 'pizza';
+
+    if (query) {
+        // 2. ) New search object and add it to state
+
+        state.search = new Search(query);
+
+        //3) prepare UI for result
+
+        //4.) search for recipe
+        await state.search.getResult()
+
+        //5.) render result on UI
+        console.log(state.search.result)
+    }
+
+
+
+
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+
+    e.preventDefault();
+
+    controllerSearch();
+
+})
+
